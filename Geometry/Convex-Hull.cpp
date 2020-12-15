4b1c9fa6a019f5ebd0143d2843a43d4592da5b64
wadidaww <59396043+wadidaww@users.noreply.github.com>
2020-12-15 15:41:27 +0700

    Update Convex-Hull.cpp

--- Geometry/Convex-Hull.cpp
+++ Geometry/Convex-Hull.cpp
@@ -1,40 +1,47 @@
-//Finding the convex hull of a set of points
-//Requires: 1D.cpp
 
-const double EPS = 0.0000001;
-vector<point> pts;
 
-vector<point> convexhull(){
-	int n = pts.size();
-	int k = 0;
-
-	if(n <= 3){
-		return pts;
-	}
-
-	vector<point> ans(2*n);
-
-	sort(pts.begin(), pts.end());
-	
-	for(int i = 0; i < n; i++){
-		while(k >= 2 && ccw(ans[k-2], ans[k-1], pts[i]) <= EPS){
-			k--;
-		}
-		ans[k++] = pts[i];
-	}
-	
-	for(int i = n-1, t = k+1; i > 0; i--){
-		while(k >= t && ccw(ans[k-2], ans[k-1], pts[i-1]) <= EPS){
-			k--;
-		}
-		ans[k++] = pts[i-1];
-	}
-	
-	ans.resize(k-1);
-	return ans;
+long long CrossProduct(pair<long long,long long> a, pair<long long,long long> b, pair<long long, long long> c)
+{
+	return (b.first - a.first) * (c.se - a.se) - (b.se - a.se) * (c.first - a.first);
 }
 
+//collinear ilang
 
-void solve(){
-	vector<point> ans = convexhull();
+vector<pair<long long,long long> > ConvexHull(vector<pair<long long,long long> > v)
+{
+	int sz = 0, n = v.size();
+	
+	if(n <= 3)
+	{
+		return v;
+	}
+	
+	vector<pair<long long,long long> >ret(2 * n);
+	
+	sort(v.begin(), v.end());
+
+	for(int i = 0 ; i < n ; i++)
+	{
+		while(sz >= 2 && CrossProduct(ret[sz - 2], ret[sz - 1], v[i]) <= 0)
+		{
+			sz--;
+		}
+		
+		ret[sz++] = v[i];
+	}
+	
+	for(int temp = sz + 1, i = n - 2 ; i >= 0 ; i--)
+	{
+		while(sz >= temp && CrossProduct(ret[sz - 2], ret[sz - 1], v[i]) <= 0)
+		{
+			sz--;
+		}
+		
+		ret[sz++] = v[i];
+	}
+	
+	
+	ret.resize(sz - 1);
+	
+	return ret;
 }
