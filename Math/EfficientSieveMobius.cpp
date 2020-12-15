7188dea96c3640c1b4c44e60d78f3bb68b4fcb38
vistokartika <38373457+vistokartika@users.noreply.github.com>
2020-12-15 19:55:18 +0700

    Update and rename EfficientSieveMobius.cpp to Mobius.cpp

--- Math/EfficientSieveMobius.cpp
+++
@@ -1,8 +0,0 @@
-prima.clear(); memset(B,true,sizeof B); B[0]=B[1]=false;
-for (ll i=2;i&lt;MAXN;i++) {
-	if (B[i]) prima.pb(i), SPF[i]=i;
-	for (ll j=0;j&lt;prima.size() &amp;&amp; i*prima[j]&lt;MAXN &amp;&amp; prima[j]&lt;=SPF[i];j++)
-	B[i*prima[j]]=false, SPF[i*prima[j]]=prima[j];}
-for (ll i=1;i&lt;MAXN;i++) {
-	if (i==1) M[i]=1; else if (SPF[i]==i) M[i]=-1;
-	else if ((i/SPF[i])%SPF[i]==0) M[i]=0; else M[i]=M[i/SPF[i]]*M[SPF[i]];}
