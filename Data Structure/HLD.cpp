5b099eeb0e0b0dd6f3f8c12ac787f052133dfdc6
Fukji <30751753+Fukji@users.noreply.github.com>
2020-12-15 09:34:22 +0700

    Update HLD.cpp
    Updated HLD.cpp

--- Data Structure/HLD.cpp
+++ Data Structure/HLD.cpp
@@ -1,79 +1,57 @@
-const int MSIZE = 1000;
-vector<pair<int, int>> adjlist[MSIZE];
-int chainhead[MSIZE], chainidx[MSIZE], pos[MSIZE];
-int parent[MSIZE], basearr[MSIZE], subsize[MSIZE];
-int ptr, chainno;
+const int MSIZE = 1e5 + 10;
+vector<int> parent, depth, heavy, head, pos, adjlist[MSIZE];
+int cur_pos;
 
-void hld(int cur, int cost, int prev){
-	if(chainhead[chainno] == -1){
-		chainhead[chainno] = cur;
-	}
-	chainidx[cur] = chainno;
-	pos[cur] = ptr;
-	basearr[ptr] = cost;
-	ptr++;
-
-	int nextcost, nextchild = -1;
-	int sz = adjlist[cur].size();
-	for(int i = 0; i < sz; i++){
-		if(adjlist[cur][i].first == prev)
-			continue;
-		if(nextchild == -1 || subsize[nextchild] < subsize[adjlist[cur][i].first]){
-			nextchild = adjlist[cur][i].first;
-			nextcost = adjlist[cur][i].second;
-		}
-	}
-
-	if(nextchild != -1){
-		hld(nextchild, nextcost, cur);
-	}
-
-	for(int i = 0; i < sz; i++){
-		if(adjlist[cur][i].first == prev || adjlist[cur][i].first == nextchild)
-			continue;
-		chainno++;
-		hld(adjlist[cur][i].first, adjlist[cur][i].second, cur);
-	}
+int dfs(int v){
+    int size = 1;
+    int max_c_size = 0;
+    for(int c : adjlist[v]){
+        if(c != parent[v]){
+            parent[c] = v, depth[c] = depth[v] + 1;
+            int c_size = dfs(c);
+            size += c_size;
+            if(c_size > max_c_size)
+                max_c_size = c_size, heavy[v] = c;
+        }
+    }
+    return size;
 }
 
-void dfs(int cur, int prev){
-	subsize[cur] = 1;
-	int sz = adjlist[cur].size();
-	for(int i = 0; i < sz; i++){
-		if(adjlist[cur][i].first == prev)
-			continue;
-		parent[adjlist[cur][i].first] == cur;
-		dfs(adjlist[cur][i].first, cur);
-		subsize[cur] += subsize[adjlist[cur][i].first];
-	}
+int decompose(int v, int h){
+    head[v] = h, pos[v] = cur_pos++;
+    if(heavy[v] != -1)
+        decompose(heavy[v], h, adj);
+    for(int c : adjlist[v]){
+        if (c != parent[v] && c != heavy[v])
+            decompose(c, c);
+    }
 }
 
-int query(int u, int v){
-	if(u == v)
-		return 0;
-	int uchain, vchain, ans;
-	vchain = chainidx[v];
-	ans = -1;
-	while(true){
-		uchain = chainidx[u];
-		if(uchain == vchain){
-			if(u == v)
-				break;
-			//query on tree operation depends on the problem description
-			//querytree(1, 0, ptr, chainpos[v]+1, chainpos[u]+1);
-			break;
-		}
-		//query on tree operation depends on the problem description
-		//querytree(1, 0, ptr, chainpos[chainhead[uchain]], chainpos[u]+1);
-		u = chainhead[uchain];
-		u = parent[u];
-	}
-	return ans;	
+int query(int a, int b){ //find maximum on path a to b
+    int res = 0;
+    for(; head[a] != head[b]; b = parent[head[b]]){
+        if (depth[head[a]] > depth[head[b]])
+            swap(a, b);
+        int cur_heavy_path_max = segment_tree_query(pos[head[b]], pos[b]); //create a segment tree DONT FORGET!!!
+        res = max(res, cur_heavy_path_max);
+    }
+    if(depth[a] > depth[b])
+        swap(a, b);
+    int last_heavy_path_max = segment_tree_query(pos[a], pos[b]);
+    res = max(res, last_heavy_path_max);
+    return res;
 }
 
-void solve(int root, int u, int v){
-	chainno = ptr = 0;
-	dfs(root, -1);
-	hld(root, -1, -1);
-	int ans = query(u, v);
+void solve(){
+    int n = adj.size();
+    parent = vector<int>(n);
+    depth = vector<int>(n);
+    heavy = vector<int>(n, -1);
+    head = vector<int>(n);
+    pos = vector<int>(n);
+    cur_pos = 0;
+
+    dfs(0, adj);
+    decompose(0, 0, adj);
+    int ans = query(0, 5);
 }
