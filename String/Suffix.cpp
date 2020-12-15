20d1af6421c5f52fd86626bb57197bc197b25e71
wadidaww <59396043+wadidaww@users.noreply.github.com>
2020-12-15 19:31:48 +0700

    Update Suffix.cpp

--- String/Suffix.cpp
+++ String/Suffix.cpp
@@ -1,22 +1,18 @@
 
-struct suffix
-{
+struct suffix{
 	int at;
 	int rank[2];
 };
 
-int Compare(suffix a, suffix b)
-{
+int Compare(suffix a, suffix b){
 	return (a.rank[0] == b.rank[0])? (a.rank[1] < b.rank[1] ? 1: 0) : (a.rank[0] < b.rank[0] ? 1: 0);
 }
 
 suffix suf[100005];
 int indexOf[100005];
 
-vector<int> buildSuffixArray(string txt, int n)
-{
-	for(int i = 0 ; i < n ; i++)
-	{
+vector<int> buildSuffixArray(string txt, int n){
+	for(int i = 0 ; i < n ; i++){
 		suf[i].at = i;
 		suf[i].rank[0] = txt[i];
 		suf[i].rank[1] = (i == n - 1 ? -1 : txt[i + 1]);
@@ -24,29 +20,24 @@ vector<int> buildSuffixArray(string txt, int n)
 
 	sort(suf, suf + n, Compare);
 
-	for(int k = 4 ; k < 2 * n ; k *= 2)
-	{
+	for(int k = 4 ; k < 2 * n ; k *= 2){
 		int rank = 0;
 		int prev_rank = suf[0].rank[0];
 		suf[0].rank[0] = 0;
 		indexOf[suf[0].at] = 0;
 		
-		for(int i = 1 ; i < n ; i++)
-		{
-			if(prev_rank == suf[i].rank[0] && suf[i].rank[1] == suf[i - 1].rank[1])
-			{
+		for(int i = 1 ; i < n ; i++){
+			if(prev_rank == suf[i].rank[0] && suf[i].rank[1] == suf[i - 1].rank[1]){
 				suf[i].rank[0] = rank;
 			}
-			else
-			{
+			else{
 				prev_rank = suf[i].rank[0];
 				suf[i].rank[0] = ++rank;
 			}
 			indexOf[suf[i].at] = i;
 		}
 		
-		for(int i = 0 ; i < n ; i++)
-		{
+		for(int i = 0 ; i < n ; i++){
 			int nextIndex = suf[i].at + k / 2;
 			suf[i].rank[1] = (nextIndex >= n ? -1 : suf[indexOf[nextIndex]].rank[0]);
 		}
@@ -62,23 +53,17 @@ vector<int> buildSuffixArray(string txt, int n)
 	return pos;
 }
 
-vector<int> kasai(string txt, vector<int> suffixArr)
-{
+vector<int> kasai(string txt, vector<int> suffixArr){
 	int n = suffixArr.size();
-
 	vector<int> lcp(n, 0);
-
 	vector<int> invSuff(n, 0);
 
 	for (int i=0; i < n; i++)
 		invSuff[suffixArr[i]] = i;
 
 	int k = 0;
-
-	for (int i = 0; i < n; i++)
-	{
-		if (invSuff[i] == n-1)
-		{
+	for (int i = 0; i < n; i++){
+		if (invSuff[i] == n-1){
 			k = 0;
 			continue;
 		}
@@ -92,6 +77,5 @@ vector<int> kasai(string txt, vector<int> suffixArr)
 
 		if (k > 0) k--;
 	}
-
 	return lcp;
 }
