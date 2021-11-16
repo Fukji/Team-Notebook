14596e53fb8db968853de5926ca64f1b88432c41
vistokartika <38373457+vistokartika@users.noreply.github.com>
2021-11-16 21:36:45 +0700

    Update Primes.cpp

--- Math/Primes.cpp
+++ Math/Primes.cpp
@@ -19,6 +19,12 @@ void sieve(int n){
 	}
 }
 
+void generate_mobius() {
+for (ll i=1;i<MAXN;i++) {
+	if (i==1) M[i]=1; else if (lpf[i]==i) M[i]=-1;
+	else if ((i/lpf[i])%lpf[i]==0) M[i]=0; else M[i]=M[i/lpf[i]]*M[lpf[i]];}
+}
+
 int phi(int n) {
     int result = n;
     for(int p = 2; p * p <= n; ++p) {
