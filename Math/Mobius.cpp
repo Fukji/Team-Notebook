09f2089f4db2cca45fe74e4ad289f52c3930a491
vistokartika <38373457+vistokartika@users.noreply.github.com>
2021-11-16 15:28:11 +0700

    Update Mobius.cpp

--- Math/Mobius.cpp
+++ Math/Mobius.cpp
@@ -1,3 +1,8 @@
-for (ll i=1;i<MAXN;i++) {
-	if (i==1) M[i]=1; else if (lpf[i]==i) M[i]=-1;
-	else if ((i/lpf[i])%lpf[i]==0) M[i]=0; else M[i]=M[i/lpf[i]]*M[lpf[i]];}
+prima.clear(); memset(B,true,sizeof B); B[0]=B[1]=false; 
+for (ll i=2;i<MAXN;i++) { 
+    if (B[i]) prima.pb(i), SPF[i]=i; 
+    for (ll j=0;j<prima.size() && i*prima[j]<MAXN && prima[j]<=SPF[i];j++) 
+    B[i*prima[j]]=false, SPF[i*prima[j]]=prima[j];} 
+for (ll i=1;i<MAXN;i++) { 
+    if (i==1) M[i]=1; else if (SPF[i]==i) M[i]=-1; 
+    else if ((i/SPF[i])%SPF[i]==0) M[i]=0; else M[i]=M[i/SPF[i]]*M[SPF[i]];} 
