ec6440d802aeada24ad5761de76d73166c2a0f7d
wadidaww <59396043+wadidaww@users.noreply.github.com>
2021-11-18 18:39:17 +0700

    Update ahoCorasick.cpp

--- String/ahoCorasick.cpp
+++ String/ahoCorasick.cpp
@@ -1,22 +1,6 @@
 //Aho-Corasick for patter searching
 //Output, 1 if exist
-
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-typedef unsigned long long ull;
-#define pii pair<int,int>
-#define pll pair<ll,ll>
-#define pb push_back
-#define fi first
-#define se second
-#define UP(a,b,c) for(ll (a)=(b);(a)<(c);(a)++)
-#define UU(a,b,c) for(ll (a)=(b);(a)<=(c);(a)++)
-#define DN(a,b,c) for(ll (a)=(b);(a)>(c);(a)--)
-#define DU(a,b,c) for(ll (a)=(b);(a)>=(c);(a)--)
-
 const int K = 256;
-
 struct Vertex {
 	int next[K];
 	bool leaf = false;
@@ -24,24 +8,20 @@ struct Vertex {
 	char pch;
 	int link = -1;
 	int go[K];
-
 	vector<int> idx;
-
 	Vertex(int p=-1, char ch='$') : p(p), pch(ch) {
 		fill(begin(next), end(next), -1);
 		fill(begin(go), end(go), -1);
 	}
 };
-
 int ans[10005];
 vector<Vertex> t(1);
 string s;
-
 void add_string(int num){
 	int v = 0;
-	for (char ch : s){
+	for(char ch : s){
 		int c = ch;
-		if (t[v].next[c] == -1){
+		if(t[v].next[c] == -1){
 			t[v].next[c] = t.size();
 			t.emplace_back(v, ch);
 		}
@@ -50,67 +30,54 @@ void add_string(int num){
     t[v].leaf = true;
     t[v].idx.pb(num);
 }
-
 int go(int v, char ch);
-
 int get_link(int v){
-	if (t[v].link == -1){
-		if (v == 0 || t[v].p == 0)
+	if(t[v].link == -1){
+		if(v == 0 || t[v].p == 0)
 			t[v].link = 0;
 		else
 			t[v].link = go(get_link(t[v].p), t[v].pch);
 	}
-	UP(i, 0, t[v].idx.size()){
-		ans[t[v].idx[i]] = 1;
+	for(int &x : t[v].idx) {
+		ans[x] = 1;
 	}
 	return t[v].link;
 }
-
 int go(int v, char ch){
 	int c = ch;
-	if (t[v].go[c] == -1){
-		if (t[v].next[c] != -1)
+	if(t[v].go[c] == -1){
+		if(t[v].next[c] != -1)
 			t[v].go[c] = t[v].next[c];
 		else
 			t[v].go[c] = v == 0 ? 0 : go(get_link(v), ch);
 	}
-	UP(i, 0, t[v].idx.size()){
-		ans[t[v].idx[i]] = 1;
+	for(int &x : t[v].idx) {
+		ans[x] = 1;
 	}
 	return t[v].go[c];
 } 
-
 string text, str;
 int q;
-
-void reset(){
+void solve(){
 	t.clear();
 	t.assign(1, Vertex());
-}
-
-void input()
-{
 	cin >> text;
 	cin >> q;
-	UU(i, 1, q){
+	for(int i = 1 ; i <= q ; ++i){
 		cin >> s;
 		add_string(i);
 		ans[i] = 0;
 	}
-
 	int node = 0;
 	char last = text[0];
-
-	UP(i, 0, text.length()){
+	for(int i = 0 ; i < text.length() ; ++i){
 		char c = text[i];
 		last = c;
 		node = go(node, c);
-		UP(x, 0, t[node].idx.size())
-		{
+		for(int x = 0 ; x < t[node].idx.size()){
 			ans[t[node].idx[x]] = 1;
 		}
 	}
-
 	if(node != 0){
 		go(node, last);
 		while(node > 0){
@@ -120,27 +87,6 @@ void input()
 			}
 		}
 	}
-
-	UU(i, 1, q)
+	for(int i = 1 ; i <= q ; ++i)
 		cout << ans[i] << endl;
-}
-
-void solve()
-{
-}
-
-void LetsRock()
-{
-	solve();
-}
-
-int main()
-{
-	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
-		reset();
-		input();
-		LetsRock();
-
-	return 0;
 }
